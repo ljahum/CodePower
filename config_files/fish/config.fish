33a653cb23eab11ec42f952a9c9356b4a14065d0
ljahum <1695325350@qq.com>
2023-12-15 16:01:13 +0800

    updates 12/15/2023 16:01:13:添加fish代理配置

--- config_files/fish/config.fish
+++ config_files/fish/config.fish
@@ -5,3 +5,9 @@ end
 
 alias cls="clear"
 alias ls="lsd"
+
+function clash
+    export http_proxy="http://192.168.244.1:7890/"
+    export https_proxy="http://192.168.244.1:7890/"
+   echo "proxy on"
+end
